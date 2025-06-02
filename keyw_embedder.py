46be7382b71051c063927a6d67b3d239cb42da37
benjamink01 <benjamin.ph.kasper@gmail.com>
2025-06-02 13:51:02 +0200

    preparation for merge

--- keyw_embedder.py
+++ keyw_embedder.py
@@ -3,6 +3,8 @@
 This keyword embedder extracts important keywords from the original query
 using Google's Gemma3 1B Parameter LLM with a system prompt. The extracted
 keywords are then embedded using OpenAI's CLIP model.
+Use:
+    python keyw_embedder.py -query "lorem ipsum" --device "mps"
 
 """
 
@@ -20,16 +22,18 @@ parser.add_argument('--device', type=str, default='cpu', help='Device on which t
 args = parser.parse_args()
 
 # Use HuggingFace transformer pipeline to feed prompt to Google Gemma 1B LLM
-print("Load Gemma model:...", end=' \r')
+print("[INFO] Load Gemma model:...", end=' \r')
 pipe = pipeline("text-generation", 
                 model="google/gemma-3-1b-it", 
                 device=args.device, 
                 torch_dtype=torch.bfloat16)
-print("Load Gemma model: success", end=' \r')
+print("[INFO] Load Gemma model: success")
 
-# System propmpt for Gemma model. Can be optimized in future (max. 32K Token context)
+# System propmpt for Gemma model. 
+# TODO: To be optimized (max. 32K Token context)
 system_prompt = "You extract important keywords or combinations of two keywords" \
-" max from a given query for an image retrieval task. Return a comma separated list."
+" max from a given query for an image retrieval task. Return a comma separated list." \
+"Under no circumstances add words to the list that are not given by the user!"
 
 query = args.query
 
@@ -50,13 +54,13 @@ output = pipe(messages, max_new_tokens=50)
 
 # Extract output string
 kw_string = output[0][0]['generated_text'][2]['content']
-print(f"Extracted keywords: {kw_string}")
+print(f"[INFO] Extracted keywords: {kw_string}")
 
 # Load CLIP model and set to inference mode
-print("Load CLIP model:...", end=' \r')
+print("[INFO] Load CLIP model:...", end=' \r')
 clip_model, preprocess_clip = clip.load("ViT-L/14@336px", device=args.device)
 clip_model.eval()
-print("Load CLIP model: success")
+print("[INFO] Load CLIP model: success")
 
 # Generate CLIP text embeddings
 def generate_text_emb(query, device):
@@ -79,6 +83,6 @@ kw_list = parse_keywords(kw_string) # get list of keywords
 query_emb = generate_text_emb(query=kw_list, device=args.device)
 
 # Save keyword embeddings
-print("Save keyword embeddings:...", end='\r')
+print("[INFO] Save keyword embeddings:...", end='\r')
 np.save("query_embeddings.npy", query_emb)
-print("Save keyword embeddings: success")
+print("[INFO] Save keyword embeddings: success")
--- matcher.py
+++ matcher.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 import argparse
 import csv
+from matching_algorithms import MeanMatcher, ParetoFrontMatcher
 
 # Argument Parser
 parser = argparse.ArgumentParser()
@@ -29,71 +30,8 @@ query_emb = np.load(args.key_emb)
 # Get paths to images
 paths = df_emb[0].tolist()
 
-# Compute similarities
-print("[INFO] Computing similarities:...", end='\r')
-similarities = img_embs @ query_emb.T
-print("[INFO] Computing similarities: success")
-
-def mean_matcher(similarities, paths):
-    """Matcher Algorithm based on mean over keywords.
-
-    Args:
-        similarities: np.array Array containing similarity scores
-        paths: List List of same lenght as similarites containing image paths
-    """
-    means = np.mean(similarities, axis=1)
-    criterion = np.percentile(means, 95)
-    print(f"[DEBUG] similarity threshold: {criterion}")
-    k = len(means[means >= criterion])
-    print(f"[INFO] Found {k} matches")
-    idcs = np.argsort(means)[::-1][:k]
-    return [paths[idx] for idx in idcs]
-
-def pareto_dominance(s):
-    """Calculates dominance matrix for Pareto Front Matching
-
-    Calculates domination of all images to each other for Pareto Front.
-
-    Args:
-        s: np.array similarities matrix of shape (n_img, n_keys)
-
-    Returns:
-        domination_mat: np.array domination matrix
-    """
-    # Compute all possible dominations_
-    # Criterion 1: i dominates j if s[i][k] >= s[j][k] for all k
-    crit1 = np.all(s[:,:,np.newaxis].repeat(s.shape[0], axis=-1) >= s.T, axis=1)
-
-    # Criterion 2: i dominates j if s[i][k] > s[j][k] for at least one k
-    crit2 = np.any(s[:,:,np.newaxis].repeat(s.shape[0], axis=-1) > s.T, axis=1)
-
-    # (crit1 and crit2) = True
-    domination_mat = np.all(np.array([crit1,crit2]), axis=0)
-
-    return domination_mat
-
-def get_pareto_front(mat):
-    """Fetches most recent Pareto Front
-    
-    """
-    # If one column only includes False the image with the column idx is not dominated by any other image
-    nondominated = np.all(mat==False, axis=0)
-    # Return idx of nondominated images
-    idx = np.where(nondominated)[0]
-    # Set rows of nondominated values to False in preparation of rerun
-    mat[idx] = False
-    return idx, mat
-
-mat = pareto_dominance(similarities)
-fronts = []
-for i in range(3):
-    idx, mat = get_pareto_front(mat=mat)
-    fronts.append(idx)
-
-# Compute mathces
-selected_imgs = [paths[idx] for idx in fronts[0]]
-print(fronts[0])
-#selected_imgs = mean_matcher(similarities, paths)
+matcher = MeanMatcher(img_embs, query_emb)
+selected_imgs = matcher.match(paths)
 
 # Wirte as .csv
 with open('matches.csv', mode="w") as file:
