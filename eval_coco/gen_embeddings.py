8037534abb6f862a202f58a732a7c56f23b6c341
Simon Hartmann <s0115@atcremers46.in.tum.de>
2025-06-13 18:28:22 +0200

    rework of embedding gen script

--- eval_coco/gen_embeddings.py
+++ eval_coco/gen_embeddings.py
@@ -14,15 +14,10 @@ PATH_EMBEDDINGS = Path("/usr/prakt/s0115/AFM_SEARCH/eval_coco/embeddings")
 PATH_IMAGES = Path("/storage/group/dataset_mirrors/old_common_datasets/coco/images/train2014")
 PATH_RESULTS = Path("eval_coco/benchmark")
 #%%
-embedder = SentenceTransformer("all-mpnet-base-v2") #best
-# model2 = SentenceTransformer("all-MiniLM-L6-v2") #5 times faster
-# embeddings = embedder.encode(sentences)
-# similarities = embedder.similarity(embeddings, embeddings)
-
-extractor = COCOCaptionExtractor(PATH_ANNOTATIONS, PATH_IMAGES)    
-captions = extractor.get_all_captions()
-#%%
-def save_embeddings_flat_hdf5(extractor, embedder, save_path):
+def save_embeddings(extractor: COCOCaptionExtractor, 
+                    embedder:SentenceTransformer, 
+                    embedding_file:Path,
+                    )->None:
     """
     Saves caption embeddings to a HDF5 file (flat structure).
     
@@ -36,9 +31,9 @@ def save_embeddings_flat_hdf5(extractor, embedder, save_path):
     """
     all_captions = extractor.get_all_captions()
     
-    filename = save_path / "caption_embeddings.h5"
+    filename = embedding_file / "caption_embeddings.h5"
     
-    print(f"Starting to save embeddings to a flat file: {filename}")
+    print(f"Starting to save embeddings to file: {filename}")
     with h5py.File(filename, 'w') as f:
         # Iterate through all image filepaths provided by the extractor
         for i, path_obj in enumerate(extractor.get_all_filepaths()):
@@ -48,7 +43,7 @@ def save_embeddings_flat_hdf5(extractor, embedder, save_path):
             # The key for captions = filename 
             caption_key = Path(path_obj).name 
             
-            # Encode captions of current image  
+            # Encode captions of current image
             embeddings = [embedder.encode(caption) for caption in all_captions[caption_key]]
             
             # Convert to a NumPy array
@@ -57,59 +52,67 @@ def save_embeddings_flat_hdf5(extractor, embedder, save_path):
             # Use the filename as the dataset key, ensuring a flat structure.
             f.create_dataset(caption_key, data=embeddings_array, compression='gzip')
     
-    print(f"\nFinished saving. All embeddings are in {filename}")
+    print(f"\nFinished saving. All embeddings keyed by the imagename are in {filename}")
     
     
     
-def load_embeddings_flat_hdf5(embedding_file_path:Path, 
-                              image_paths:List[Path]=None):
+def load_embeddings(embedding_file_path:Path, 
+                    image_paths:List[Path]=None,
+                    )->Dict[str, np.ndarray]:
     """
     Loads embeddings from a flat HDF5 file.
     
     Args:
-        save_path (Path): The directory where the HDF5 file is stored.
+        save_path (Path): Path of the HDF5 file.
         image_paths (list, optional): A list of specific image paths/filenames to load.
                                       If None, all embeddings are loaded. Defaults to None.
     
     Returns:
         dict: A dictionary mapping image filenames to their embedding arrays.
     """
-    filename = embedding_file_path / "caption_embeddings.h5"
     
-    if not filename.exists():
-        raise FileNotFoundError(f"HDF5 file not found at: {filename}")
+    if not embedding_file_path.exists():
+        raise FileNotFoundError(f"HDF5 file not found at: {embedding_file_path}")
 
     result = {}
-    with h5py.File(filename, 'r') as f:
+    with h5py.File(embedding_file_path, 'r') as f:
         if image_paths is None:
             # Load all embeddings
-            print(f"Loading all {len(f.keys())} images with each 5 embeddings from {filename}...")
+            print(f"Loading all {len(f.keys())} images with each 5 embeddings from {embedding_file_path}...")
             for key in f.keys():
                 result[key] = f[key][:] # Read data into memory
         else:
             # Load only the specified embeddings
-            print(f"Loading {len(image_paths)} specific embeddings from {filename}...")
+            print(f"Loading {len(image_paths)} specific images with each 5 embeddings from {embedding_file_path}...")
             for path in image_paths:
                 key = Path(path).name  # Extract filename to use as the key
                 if key in f:
                     result[key] = f[key][:]
                 else:
-                    print(f"Warning: Embedding for '{key}' not found in HDF5 file.")
+                    print(f"Warning: Key for image '{key}' not found in HDF5 file.")
     
-    print(f"Successfully loaded {len(result)} embeddings into memory.")
+    print(f"Successfully loaded {len(result)} images with each 5 embeddings into memory.")
     return result
 
 # %%
 # test it 
+#%%
+embedder = SentenceTransformer("all-mpnet-base-v2") #best
+# model2 = SentenceTransformer("all-MiniLM-L6-v2") #5 times faster according to the internet
+# embeddings = embedder.encode(sentences)
+# similarities = embedder.similarity(embeddings, embeddings)
 
-# save_embeddings_flat_hdf5(extractor, embedder, PATH_EMBEDDINGS)
-# # %%
-# import time
-# start = time.time()
-# all_embeddings = load_embeddings_flat_hdf5(PATH_EMBEDDINGS)
-# print(f"Loaded {len(all_embeddings)} total embeddings.")
-# print(f"Time taken: {time.time() - start:.2f} seconds")
-# #%%
+extractor = COCOCaptionExtractor(PATH_ANNOTATIONS, PATH_IMAGES)    
+captions = extractor.get_all_captions()
+
+save_embeddings(extractor, embedder, PATH_EMBEDDINGS)
+# %%
+import time
+start = time.time()
+all_embeddings = load_embeddings(PATH_EMBEDDINGS)
+print(f"Loaded {len(all_embeddings)} total embeddings.")
+print(f"Time taken: {time.time() - start:.2f} seconds")
+#%%
 
 # some_images_to_load = [
 #     'COCO_train2014_000000000009.jpg',
