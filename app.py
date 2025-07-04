8c45d2e1cb9a2f0e98617b4b47e187bb8778a66d
Benjamin Kasper <s0122@atcremers45.in.tum.de>
2025-07-04 20:47:44 +0200

    update/delete feature

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import time
 import uuid
 
 class ImageRetrievalApp:
-    """Gradio UI for retrieval pipeline.
-    This implements the Gradio UI for the image retrieval pipeline. 
-    If used on a SLURM Cluster this app need to be run on an entry node.
-
+    """Class for Gradio UI of the image/video retrieval pipeline.
+    This class has a lot of extra functionality to allow for it to run on 
+    a SLURM cluster with no port forwarding between compute and entry node.
+    This app need to be run on an entry node!
     """
     def __init__(self, port:int=7860):
         
@@ -19,18 +19,19 @@ class ImageRetrievalApp:
             self._tmp_dir = tmp_dir
         except OSError as e:
             raise OSError("Failed to create a tmp folder that is needed for Gradio") from e
-        
+
+        # set env variables to tmp folder
         os.environ["GRADIO_TEMP_DIR"] = self._tmp_dir
         os.environ["TMPDIR"] = self._tmp_dir
         os.environ["TEMP"] = self._tmp_dir
         os.environ["TMP"] = self._tmp_dir
 
         self.port = port # port the gradio ui can be accessed
-        self.gallery_dir = "/usr/prakt/s0122/afm/dataset/flickr8k/Flicker8k_Dataset/"
-        self._suporrted_formats = ('.png', '.jpeg', '.jpg')
-        self._batchsize = 20 # number of images to load at once
+        self.gallery_dir = "/usr/prakt/s0122/afm/AFM_SEARCH/tmp/" # directory where the gallery is located
+        self._suporrted_formats = ('.png', '.jpeg', '.jpg') # list of supported file formats
+        self._batchsize = 20 # number of images to load at once (adjust according to system)
         self._allowed_paths = [self._tmp_dir, self.gallery_dir] # paths gradio is allowed to access outside working dir
-
+        
         # Paths to all items in the gallery
         self._all_paths = sorted([
             os.path.join(self.gallery_dir, item) 
@@ -38,24 +39,42 @@ class ImageRetrievalApp:
             if item.lower().endswith(self._suporrted_formats)
         ])
 
-        self.app = self.build_interface()
+        self.app = self.build_interface() # build app
 
+    # ======================================================================= #
+    # This section contains methods for load more feature
 
     def _load_batch(self, batch_idx, current_items):
         """Returns a new batch of items and new batch index."""
         start = batch_idx * self._batchsize
         end = start + self._batchsize
         new_batch = self._all_paths[start:end]
-        return current_items + new_batch, batch_idx + 1
+        status = f"✅ Loaded {len(new_batch)} more items" if len(new_batch) > 0 else "⚠️ No more items to load" 
+        return current_items + new_batch, batch_idx + 1, status
 
+    # ======================================================================= #
+    # This section contains methods for the cancel search feature
 
     def _reset_gallery(self):
-        """Method to reset the Gallery after cancelling a completed search."""
+        """Reset the Gallery after cancelling a completed search."""
         initial_batch = self._all_paths[:self._batchsize]
         self.batch_index.value = 1
         gallery_title = "## 🎞️ Camera Roll"
         return initial_batch, self.batch_index.value, gallery_title
     
+    def _on_search_or_cancel(self, prompt, current_items, btn_label):
+        """Switches functionality of the search button between search and cancel."""
+        if btn_label == "Search":
+            results, new_batch_idx, new_title = self._search_items(prompt, current_items)
+            return results, new_batch_idx, new_title, gr.update(value="Cancel"), "Cancel"
+        else:
+            results, new_batch_idx, new_title = self._reset_gallery()
+            # TODO: clear search input as well
+            return results, new_batch_idx, new_title, gr.update(value="Search"), "Search"
+
+
+    # ======================================================================= #
+    # This section contains methods for the search feature.
 
     def _request_pipeline(self, file_path, query):
         """Write prompt to .json file to request retrieved images from pipeline.
@@ -82,7 +101,6 @@ class ImageRetrievalApp:
                 json.dump(data, file, indent=2)
             return None
 
-  
     def _wait_for_response(self, file_path, query, check_interval=0.1):
         """Waits for the results to be written into the json"""
         while True:
@@ -103,7 +121,6 @@ class ImageRetrievalApp:
                 return data[query]
             else:
                 time.sleep(check_interval)  # wait 100ms before checking again
-
 
     def _search_items(self, prompt, current_items):
         """Central search function of the UI."""
@@ -126,73 +143,36 @@ class ImageRetrievalApp:
         # return matches to be displayed, (batch_idx), new title containing no. of matches
         return current_items, 1, gallery_title
     
+    # ======================================================================= #
+    # This section contains methods for the delete feature
+
+    def _on_gallery_select(self, evt: gr.SelectData):
+        """EventListener for item selection in the gallery. Important for delete function"""
+        selected_item_path = evt.value
+        print(f"Selected image: {selected_item_path}")
+        return selected_item_path
+       
+    def _delete_selected_item(self, selected_item):
+        """Deletes selected items from disk and updates the gallery"""
+        selected_item_path = os.path.join(self.gallery_dir, selected_item['image']['orig_name']) # TODO: add video support
+
+        if selected_item_path and os.path.exists(selected_item_path):
+            try:
+                os.remove(selected_item_path)
+                self._all_paths.remove(selected_item_path)
+                updated_batch = self._all_paths[:self._batchsize]
+                return updated_batch, f"✅ Deleted {os.path.basename(selected_item_path)} items(s).", 1
+            except Exception as e:
+                return gr.update(), f"❌ Failed to delete: {str(e)}", gr.update()
+        return gr.update(), "⚠️ No valid item selected.", gr.update()
     
-    def _on_search_or_cancel(self, prompt, current_items, btn_label):
-        """Switches functionality of the search button between search and cancel."""
-        if btn_label == "Search":
-            results, new_batch_idx, new_title = self._search_items(prompt, current_items)
-            return results, new_batch_idx, new_title, gr.update(value="Cancel"), "Cancel"
-        else:
-            results, new_batch_idx, new_title = self._reset_gallery()
-            # Optionally clear search input as well
-            return results, new_batch_idx, new_title, gr.update(value="Search"), "Search"
-
-
-    def build_search_tab(self):
-        """Search/Gallery Tab UI implementation."""
-        with gr.Tab("Search"):
-            # Title to display: Displays number of matches after each search
-            gallery_title = gr.Markdown("## 🎞️ Gallery")
-
-            with gr.Row():
-                self.search_input = gr.Textbox(
-                    label="Search", 
-                    placeholder="Search for anyting...", 
-                    scale=4
-                )
-                self.search_btn = gr.Button(
-                    "Search",
-                    scale=1,
-                    size="lg"
-                )
-            
-            # Batch of images to display
-            initial_batch = self._all_paths[:self._batchsize]
-            self.batch_index = gr.State(1)  # start after first batch
-            
-            self.btn_label = gr.State("Search") # state for the search/cancel button
-
-            self.gallery = gr.Gallery(
-                label=" ",
-                value=initial_batch,
-                columns=4,
-                height="auto"
-            )
-
-            self.load_more_btn = gr.Button("Load More Images")
-            # Bind load more button
-            self.load_more_btn.click(
-                fn=self._load_batch,
-                inputs=[self.batch_index, self.gallery],
-                outputs=[self.gallery, self.batch_index]
-            )   
-
-            self.search_btn.click(
-                fn=self._on_search_or_cancel,
-                inputs=[self.search_input, self.gallery, self.btn_label],
-                outputs=[
-                    self.gallery, # update gallery
-                    self.batch_index, # handle batch index TODO: relevant for results?
-                    gallery_title, # update gallery title to display no. of matches
-                    self.search_btn, # update text on the search button (search vs. cancel)
-                    self.btn_label  # update state of the search/cancel button
-                ]
-            )
+    # ======================================================================= #
+    # This section contains methods for the upload feature
 
     def _handle_upload(self, uploaded_files):
         """Handles the upload logic of the UI"""
         if not uploaded_files:
-            return "⚠️ No files selected."
+            return "⚠️ No files selected.", gr.update(), gr.update()
 
         uploaded_paths = []
 
@@ -211,23 +191,100 @@ class ImageRetrievalApp:
         
             try:
                 os.rename(file, target_path)  # move uploaded tmp file to gallery dir
-                uploaded_paths.append(filename)
+                uploaded_paths.append(os.path.join(self.gallery_dir, filename))
             except Exception as e:
-                return f"❌ Failed to upload {filename}: {str(e)}"
+                return f"❌ Failed to upload {filename}: {str(e)}", gr.update(), gr.update()
 
         if uploaded_paths: # uploaded items successfully
             self._all_paths.extend(uploaded_paths)
             self._all_paths = sorted(self._all_paths)
-            return f"✅ Uploaded: {len(uploaded_paths)} item."
+            return f"✅ Uploaded: {len(uploaded_paths)} item(s).", self._all_paths[:self._batchsize], 1
         else:
-            return "⚠️ No valid items uploaded."
+            return "⚠️ No valid items uploaded.", self._all_paths[:self._batchsize], 1
+
+    # ======================================================================= #
+    # This section contains methods to build the Search, Upload and Settings page
+    
+    def build_search_tab(self):
+        """Search/Gallery Tab UI implementation."""
+        with gr.Tab("Search"):
+            # Title to display: Displays number of matches after each search
+            gallery_title = gr.Markdown("## 🎞️ Camera Roll")
+
+            with gr.Row():
+                self.search_input = gr.Textbox(
+                    placeholder="Search for anyting...", 
+                    scale=4,
+                    show_label = False
+                )
+                self.search_btn = gr.Button(
+                    "Search",
+                    scale=1,
+                    size="lg"
+                )
+            
+            # Batch of images to display
+            initial_batch = self._all_paths[:self._batchsize]
+            self.batch_index = gr.State(1)  # start after first batch
+            
+            self.btn_label = gr.State("Search") # state for the search/cancel button
+
+            self.selected_item = gr.State("") # state to collect selected item
+
+            self.gallery = gr.Gallery(
+                label=" ",
+                value=initial_batch,
+                columns=4,
+                height="auto",
+                interactive = False
+            )
+
+            with gr.Row():
+                self.load_more_btn = gr.Button("Load More Images")
+                self.delete_btn = gr.Button("Delete Selected Item")
+
+            self.status_bar = gr.Textbox(visible=True, interactive=False, label="System Status")
+            
+            # Bind load more button
+            self.load_more_btn.click(
+                fn=self._load_batch,
+                inputs=[self.batch_index, self.gallery],
+                outputs=[self.gallery, self.batch_index, self.status_bar]
+            )   
+
+            self.gallery.select(
+                fn=self._on_gallery_select,
+                inputs=[],
+                outputs=self.selected_item
+            )
+
+            # Bind delete button
+            self.delete_btn.click(
+                fn=self._delete_selected_item,
+                inputs=[self.selected_item],
+                outputs=[self.gallery, self.status_bar, self.batch_index]
+            )
+
+
+            # Bind search/cancel button
+            self.search_btn.click(
+                fn=self._on_search_or_cancel,
+                inputs=[self.search_input, self.gallery, self.btn_label],
+                outputs=[
+                    self.gallery, # update gallery
+                    self.batch_index, # handle batch index TODO: relevant for results?
+                    gallery_title, # update gallery title to display no. of matches
+                    self.search_btn, # update text on the search button (search vs. cancel)
+                    self.btn_label  # update state of the search/cancel button
+                ]
+            )
 
     def build_upload_tab(self):
         """Upload tab UI implementation."""
         with gr.Tab("Upload"):
             gr.Markdown("## ⬆️ Upload Images to Gallery")
 
-            uploader = gr.File(
+            upload_mask = gr.File(
                 file_types=["image"],
                 file_count="multiple",
                 label="Select Images to Upload",
@@ -242,8 +299,8 @@ class ImageRetrievalApp:
 
             upload_btn.click(
                 fn=self._handle_upload,
-                inputs=[uploader],
-                outputs=[upload_status]
+                inputs=[upload_mask],
+                outputs=[upload_status, self.gallery, self.batch_index]
             ) 
 
     def build_settings_tab(self):
